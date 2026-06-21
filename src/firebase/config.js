69459c8d65cb8e003e40aeb3586de58c76c61d50
paullenguax <paul@lenguax.com>
2026-06-21 20:57:34 +0100

    new

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDtWlmww9iNBcLnfzoJE19j0tq_MsmTOkk',
-  authDomain: 'raterscores.firebaseapp.com',
-  projectId: 'raterscores',
-  storageBucket: 'raterscores.firebasestorage.app',
-  messagingSenderId: '72763874219',
-  appId: '1:72763874219:web:d51c927999497ab76450ee',
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
